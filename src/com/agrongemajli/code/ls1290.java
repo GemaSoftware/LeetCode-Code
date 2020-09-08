a7e410e3b1234f7008d72e10e89111cb82bc1dac
Agron Gemajli <agrongemajli@outlook.com>
2020-09-08 00:12:01 -0400

    1290

---
+++ src/com/agrongemajli/code/ls1290.java
@@ -0,0 +1,28 @@
+package com.agrongemajli.code;
+
+public class ls1290 {
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+//runtime 0ms.
+class Solution1290 {
+    public int getDecimalValue(ListNode head) {
+        StringBuilder holder = new StringBuilder();
+        while(head.next != null){
+            holder.append(String.valueOf(head.val));
+            head = head.next;
+        }
+        holder.append(String.valueOf(head.val));
+
+        return Integer.parseInt(holder.toString(), 2);
+    }
+}
